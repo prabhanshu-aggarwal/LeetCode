6221812d222e45c9ab7448ef301ea78e4c9cfe81
Prabhanshu Aggarwal <30840229+prabhanshu-aggarwal@users.noreply.github.com>
2024-04-04 17:57:32 -0400

    Update Fibonacci.py

--- Recursion1/Fibonacci.py
+++ Recursion1/Fibonacci.py
@@ -8,3 +8,18 @@ class Solution(object):
             return n
         else:
             return self.fib(n-1)+self.fib(n-2)
+
+
+#Using Memoization
+class Solution(object):
+    def __init__(self):
+        self.memo={}
+    def fib(self, n):
+        if n<=1:
+            return n
+        if n not in self.memo:
+            self.memo[n] = self.fib(n-1)+self.fib(n-2)
+        return self.memo[n]
+      
+sol = Solution()
+print(sol.fib(4))
